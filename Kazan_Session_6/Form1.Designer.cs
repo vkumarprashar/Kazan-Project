d4a93dda2e44678d2a39b4205a78ef241d87490a
Vinay Kumar Prashar <vkumarprashar@users.noreply.github.com>
2022-04-27 22:46:38 +0530

    First commit

---
+++ Kazan_Session_6/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Kazan_Session_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.invControlBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chart1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // chart1
+            // 
+            resources.ApplyResources(this.chart1, "chart1");
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea3.Name = "ChartArea1";
+            chartArea3.Position.Auto = false;
+            chartArea3.Position.Height = 90F;
+            chartArea3.Position.Width = 80F;
+            chartArea3.Position.X = 3F;
+            chartArea3.Position.Y = 9F;
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView2);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView2, "dataGridView2");
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dataGridView3);
+            resources.ApplyResources(this.groupBox4, "groupBox4");
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.TabStop = false;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView3, "dataGridView3");
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            // 
+            // invControlBtn
+            // 
+            resources.ApplyResources(this.invControlBtn, "invControlBtn");
+            this.invControlBtn.Name = "invControlBtn";
+            this.invControlBtn.UseVisualStyleBackColor = true;
+            // 
+            // closeBtn
+            // 
+            resources.ApplyResources(this.closeBtn, "closeBtn");
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.chart2);
+            resources.ApplyResources(this.groupBox5, "groupBox5");
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.TabStop = false;
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea2";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            resources.ApplyResources(this.chart2, "chart2");
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea2";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series4.SmartLabelStyle.IsOverlappedHidden = false;
+            this.chart2.Series.Add(series4);
+            // 
+            // Column1
+            // 
+            resources.ApplyResources(this.Column1, "Column1");
+            this.Column1.Name = "Column1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.invControlBtn);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button invControlBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ Kazan_Session_6/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Kazan_Session_6
+{
+    public partial class Form1 : Form
+    {
+        KazanSession6Entities entities;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                    break;
+                case 1:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("hi-IN");
+                    break;
+                case 3:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("pa-IN");
+                    break;
+                default:
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                    break;
+            }
+
+            this.Controls.Clear();
+            InitializeComponent();
+
+            Form1_Load(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            entities = new KazanSession6Entities();
+            dataGridView1.DataSource = entities.OrderItems
+                .GroupBy(x => new {
+                    x.Order.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name,
+                    x.Order.Date.Month
+                }).Select(x => new
+                {
+                    Department = x.Key.Name,
+                    Month = x.Key.Month,
+                    Count = x.Sum(g => g.UnitPrice * g.Amount)
+                }).ToList();
+
+            //Pie Chart
+            chart1.DataSource = entities.OrderItems
+                 .GroupBy(x => new {
+                     x.Order.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name
+                 }).Select(x => new
+                 {
+                     Department = x.Key.Name,
+                     Count = x.Sum(g => g.UnitPrice * g.Amount)
+                 }).ToList();
+
+            chart1.Series["Series1"].XValueMember = "Department";
+            chart1.Series["Series1"].YValueMembers = "Count";
+            chart1.Series["Series1"].IsValueShownAsLabel = true;
+
+            //Pie Chart
+            chart2.DataSource = entities.OrderItems
+                 .GroupBy(x => new {
+                     x.Order.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name,
+                     x.Order.Date.Year
+                 }).Select(x => new
+                 {
+                     Department = x.Key.Name,
+                     Month = x.Key.Year,
+                     Count = x.Sum(g => g.UnitPrice * g.Amount)
+                 }).ToList();
+
+            chart2.Series["Series1"].XValueMember = "Month";
+            chart2.Series["Series1"].YValueMembers = "Count";
+//            chart2.Series["Series1"].IsValueShownAsLabel = true;
+            chart2.ChartAreas["ChartArea2"].AxisX.ScrollBar.Enabled = true;
+
+
+
+            //DataGrid2
+            dataGridView2.DataSource = entities.OrderItems
+                .GroupBy(x => new {
+                    x.Part.Name,
+                    x.Order.Date.Month
+                })
+                .Select(x => new
+                {
+                    PartName = x.Key.Name,
+                    Month = x.Key.Month,
+                    PartSum = x.Max(y => y.UnitPrice * y.Amount),
+                    //x.Max(g => g.UnitPrice * g.Amount),
+                    PartCount = x.Max(g => g.UnitPrice)
+                }).ToList();
+
+            entities.OrderItems
+                .GroupBy(x => new {
+                    x.Part.Name,
+                    x.Order.Date.Month
+                }).Select(x => new
+                {
+                    PartName = x.Key.Name,
+                    Month = x.Key.Month,
+                    PartCount = x.Max(g => g.UnitPrice)
+                }).ToList();
+
+
+            //Datagridview 3
+            entities.OrderItems
+                .GroupBy(x => new {
+                    x.Order.EmergencyMaintenance.Asset.AssetName,
+                    x.Order.Date.Month
+                }, (key,group) => new {
+                    Asset_Name = key.AssetName
+                }).ToList();
+
+            //Setting datagridview1 one data
+            var list= entities.OrderItems
+                .Select(a => new
+                {
+                    Month = a.Order.Date.Month,
+                    Year = a.Order.Date.Year,
+                    Amount = a.UnitPrice * a.Amount,
+                    Department = a.Order.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name
+                }).GroupBy(x => new
+                {
+                    year = x.Year, 
+                    month = x.Month,
+                    dept = x.Department
+                }, (a,b) => new
+                {
+                    date = a.month + "-" + a.year,
+                    amount = b.Sum(c=> c.Amount),
+                    department =  a.dept 
+                }).ToList();
+
+            
+            var months = entities.OrderItems
+             .Select(a => new
+             {
+                 Month = a.Order.Date.Month,
+                 Year = a.Order.Date.Year
+                 
+             }).OrderBy(x=> x.Year).OrderBy(x => x.Month).GroupBy(x => new
+             {
+                 year = x.Year,
+                 month = x.Month
+                 
+             }, (a, b) => new
+             {
+                 date = a.month + "-" + a.year                 
+                
+             }).Distinct().ToList();
+            
+            DataGridViewTextBoxColumn cell1 = new DataGridViewTextBoxColumn();
+            cell1.Name = "Department/Month";
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            cell1});
+            foreach (var m in months)
+            {
+                DataGridViewTextBoxColumn cell = new DataGridViewTextBoxColumn();
+                cell.Name = m.date;
+                Console.WriteLine("Date: " + m.date);
+                this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            cell});
+            }
+            var department = entities.OrderItems
+             .GroupBy(x => new
+             {
+                 dept = x.Order.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name
+
+             }).Select(a => new
+             {
+                 Department = a.Key.dept
+
+             }).OrderBy(x => x.Department).Distinct().ToList();
+            foreach (var d in department)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(this.dataGridView3);
+                row.Cells[0].Value = d.Department;
+                Console.WriteLine("Date: " + d.Department);
+                this.dataGridView3.Rows.Add(row);
+            }
+
+            foreach(var l in list)
+            {
+                for(int i = 1; i < dataGridView3.Columns.Count; i++)
+                {
+                    Console.WriteLine("Row count: " + dataGridView3.Rows.Count);
+                    for (int j = 1; j < dataGridView3.Rows.Count; j++)
+                    {
+
+                        if (dataGridView3.Rows[j].Cells[0].Value != null) {
+                        if (l.date == dataGridView3.Columns[i].Name && l.department == dataGridView3.Rows[j].Cells[0].Value.ToString())
+                        {
+                            dataGridView3.Rows[j].Cells[i].Value = l.amount;
+                        }
+                    }
+                    }
+                }
+            }
+
+            //Select(x => new
+            //{
+            //     Asset_Name = x.Key.AssetName,
+            //     Month = x.Key.Month,
+            //    Department = x.Select(g => g.Order.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name),
+            //   EM_EndDate = x.Select(g => g.Order.EmergencyMaintenance.EMEndDate)
+            //})
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
